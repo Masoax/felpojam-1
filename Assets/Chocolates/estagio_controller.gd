4a2058fc82d88d9564c9478663adec4065545922
RodrigoAmaral <rodrigoamaral1705@gmail.com>
2026-02-22 21:35:16 -0300

    add assets faltantes

--- Assets/Chocolates/estagio_controller.gd
+++ Assets/Chocolates/estagio_controller.gd
@@ -1,3 +1,15 @@
+#Olhar se grupos (groups) ou camadas de colisão (collision layers/masks) são úteis para identificar qual prensa/cobertura/carimbo acertou o chocolate
+#Ver questão de animação para o dispensers de cobertura, algum splash (olhar jogo da gatinha)
+#Verificar qual forma prensou (utilizar o nome/id_unico da prensa)
+#	somar o valor na variavel tipo_chocolate
+#	alterar sprite para o correspondente
+#Realizar o mesmo para as coberturas e para o carimbo
+#Verificar se o cholate passou pela area das prensas sem ser prensado (desabilitar colisão)
+#	realizar o mesmo para a cobertura e carimbo
+#Utilizar do mesmo sistema das prensas para os dispensers e carimbos
+#NO carimbo tem uma chance dele criar uma com a cara do felps/dourado, conta como sucesso sempre
+#Na checagem verificar tanto o estagio quanto o tipo
+#Animação de falha pode ser tanto ele cair na lixeira quanto explodir (segundo só se a da lixeira ficar esquisita)
 extends Node2D
 
 #constantes estagios do chocolate
@@ -46,53 +58,40 @@ const CARIMBADO_CARINHA: int = 41
 const CARIMBADO_MEIAUM:  int = 42
 const CARIMBADO_BELA:    int = 43
 
-@onready var area_colisao: Area2D = $Sprite2D/Area2D
-@onready var label: Label = $Label
+@onready var sprite:      Sprite2D = $Sprite2D
+@onready var area_colisao:  Area2D = $Sprite2D/Area2D
 @onready var condicao_sucesso: int = 0
-@onready var estagio: int = MASSA
-@onready var tipo_chocolate: int = MASSA
+@onready var estagio:          int = MASSA
+@onready var tipo_chocolate:   int = MASSA
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area_colisao.get_overlapping_areas().size() == 2 && condicao_sucesso:
-		#Olhar se grupos (groups) ou camadas de colisão (collision layers/masks) são úteis para identificar qual prensa/cobertura/carimbo acertou o chocolate
-		#Ver questão de animação para o dispensers de cobertura, algum splash (olhar jogo da gatinha)
-		#Verificar qual forma prensou (utilizar o nome/id_unico da prensa)
-		#	somar o valor na variavel tipo_chocolate
-		#	alterar sprite para o correspondente
-		#Realizar o mesmo para as coberturas e para o carimbo
-		#Verificar se o cholate passou pela area das prensas sem ser prensado (desabilitar colisão)
-		#	realizar o mesmo para a cobertura e carimbo
-		#Utilizar do mesmo sistema das prensas para os dispensers e carimbos
-		#NO carimbo tem uma chance dele criar uma com a cara do felps/dourado, conta como sucesso sempre
-		#Na checagem verificar tanto o estagio quanto o tipo
-		#Animação de falha pode ser tanto ele cair na lixeira quanto explodir (segundo só se a da lixeira ficar esquisita)
-		
 		print("sucesso")
-		set_estagio(FORMADO)
+		set_estagio(FORMADO, FORMADO_QUADRADO)
 		condicao_sucesso = 0
 	else:
 		condicao_sucesso += 1
-		set_estagio(DEFORMADO)
+		set_estagio(DEFORMADO, DEFORMADO_DIR)
 		print("deformou")
 	print(condicao_sucesso)
 
 func reseta_chocolate() -> void:
 	condicao_sucesso = 0
-	set_estagio(MASSA)
+	set_estagio(MASSA, MASSA)
 	set_monitorable(true)
 
 func set_monitorable(condicao: bool) -> void:
 	area_colisao.monitorable = condicao
 
-func set_estagio(estagio_arg: int) -> void:
+func set_estagio(estagio_arg: int, tipo_arg: int) -> void:
 	estagio = estagio_arg
-	if estagio == MASSA:
-		label.text = "MASSA"
-	elif estagio == DEFORMADO:
-		label.text = "DEFORMADO"
-	elif estagio == FORMADO:
-		label.text = "FORMADO"
-	elif estagio == COBERTO:
-		label.text = "COBERTO"
-	else:
-		label.text = "CARIMBADO"
+	#if estagio == MASSA:
+		#label.text = "MASSA"
+	#elif estagio == DEFORMADO:
+		#label.text = "DEFORMADO"
+	#elif estagio == FORMADO:
+		#label.text = "FORMADO"
+	#elif estagio == COBERTO:
+		#label.text = "COBERTO"
+	#else:
+		#label.text = "CARIMBADO"
